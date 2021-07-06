931f823bab429ab8de02d2332e21caa1d7124968
ImamAlfarizi <imam.alfarizi.777@gmail.com>
2021-07-06 07:47:27 +0700

    show category lists in add post page

---
+++ resources/views/posts/_listCategory.blade.php
@@ -0,0 +1,17 @@
+<ul class="pl-1 my-1" style="list-style: none;">
+    @foreach ($categories as $category)
+        <li class="form-group form-check my-1">
+            <input 
+                class="form-check-input"
+                type="checkbox" 
+                value="{{ $category->id }}"
+                name="category[]">
+            <label class="form-check-label">{{ $category->title }}</label>
+            @if ($category->herit)
+                @include('posts._listCategory', [
+                    'categories' => $category->herit 
+                ])
+            @endif
+        </li>
+    @endforeach
+</ul>
