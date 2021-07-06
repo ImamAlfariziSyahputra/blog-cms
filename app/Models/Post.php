762ec9a3dc083de78bf9c4ea4beb4a3f18abb712
ImamAlfarizi <imam.alfarizi.777@gmail.com>
2021-07-06 19:07:52 +0700

    insert data logic post table (add post)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'thumbnail', 'description', 'content', 'status', 'user_id'];
+
+    public function tag()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
+
+    public function category()
+    {
+        return $this->belongsToMany(Category::class)->withTimestamps();
+    }
 }
